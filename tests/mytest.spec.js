cd55accec57c7adcac18c7e098ff7899cdc0b84c
jawadchy2150 <jawadchowdhury260@gmail.com>
2025-03-10 23:04:29 +0600

    done with codegen

---
+++ tests/mytest.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/index.html');
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.locator('#loginusername').click();
+  await page.locator('#loginusername').fill('jawad123');
+  await page.locator('#loginusername').press('Tab');
+  await page.locator('#loginpassword').fill('jawad123');
+  await page.getByRole('button', { name: 'Log in' }).click();
+});
