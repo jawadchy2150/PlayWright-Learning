9b8b9da60afabaf10c4b7a610f051a8918e9e6bb
jawadchy2150 <jawadchowdhury260@gmail.com>
2025-03-12 23:57:29 +0600

    assertion going on

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
+import { log } from 'console';
 
 test('AssertionsTest', async ({ page}) => {
 
-    await page.goto('https://www.nopcommerce.com/en/register')
+    await page.goto('https://www.wikipedia.org/')
      
-    await expect(page).toHaveURL('https://www.nopcommerce.com/en/register')
+    await expect(page).toHaveURL('https://www.wikipedia.org/')
 
-    await expect(page).toHaveTitle("Register - nopCommerce")
+    // await expect(page).toHaveTitle("Register - nopCommerce")
+
+    const logoElement = await page.locator('.central-textlogo')
+    await expect(logoElement).toBeVisible()
 })
